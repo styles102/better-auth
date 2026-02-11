49ca7d711b077a670e4319d95a820df114d5b1a9
Liam Styles <liam.styles@distinction.co.uk>
2026-02-11 14:02:19 +0000

    Implement authentication check in dashboard page

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,5 +1,20 @@
-export default function Dashboard() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { unauthorized } from "next/navigation";
+
+export default async function Dashboard() {
+	const session = await auth.api.getSession({
+		headers: await headers()
+	})
+	
+	if(!session) {
+		unauthorized();
+	}
+
 	return (
-		<h1>Dashboard</h1>
+		<div>
+			<h1>Welcome to the Dashboard</h1>
+			<p>Hi, {session!.user.name}</p>
+		</div>
 	)
 }
