2d6ca7bff66eec7ff6a74c1f054ebd25b1734c5b
Liam Styles <liam.styles@distinction.co.uk>
2026-02-11 13:39:02 +0000

    Add email verification page

---
+++ src/app/(non-auth)/email-verification/page.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/lib/utils";
+
+export default function EmailVerificationPage() {
+	return (
+		<div className={cn("place-items-center")}>
+			<h2>We&aspos;ve sent a verification email to you. Please follow the instructions to sign in to your account.</h2>
+		</div>
+	);
+}
